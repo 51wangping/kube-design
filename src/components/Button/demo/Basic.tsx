3e5e413619e95f364950b0b9104a661e15c419ef
51wangping <253423846@qq.com>
2022-12-22 11:19:45 +0800

    fix: theme style

--- src/components/Button/demo/Basic.tsx
+++ src/components/Button/demo/Basic.tsx
@@ -1,10 +1,16 @@
-import { Checkbox } from '@kube-design/components';
+import { Button } from '@kube-design/components';
 import React from 'react';
 
 export default function Basic() {
   return (
     <div>
-      <Checkbox> Checkbox </Checkbox>
+      <Button type="primary">Kube Design</Button>
+      <Button type="control">Kube Design</Button>
+      <Button>Kube Design</Button>
+      <Button type="danger">Kube Design</Button>
+      <Button type="flat">Kube Design</Button>
+      <Button disabled>Kube Design</Button>
+      <Button ghost>Kube Design</Button>
     </div>
   );
 }
